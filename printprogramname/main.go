e1a3059927c112cd6ea1bb99ce35ecc4696ef71f
Nouaman Lazar <140210801+lazar1234lazar@users.noreply.github.com>
2024-01-19 00:39:58 +0000

    Update main.go

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"os"
-
 	"github.com/01-edu/z01"
 )
 
 func main() {
+	// Get the command-line arguments (including the program name)
 	result := os.Args
+
+	// Iterate through the characters of the program name
 	for j, i := range result[0] {
+		// Skip the first two characters (program name and space)
 		if j > 1 {
+			// Print each character after the first two
 			z01.PrintRune(i)
 		}
 	}
+
+	// Print a newline character at the end
 	z01.PrintRune(10)
 }
